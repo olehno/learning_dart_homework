5116a624600fc275bebfb68ce55b3e6757a4dea1
Arthur <olehno.arturs@gmail.com>
2023-06-11 19:20:21 +0300

    Initial commit

---
+++ bin/learning_dart_homework.dart
@@ -0,0 +1,5 @@
+import 'package:learning_dart_homework/learning_dart_homework.dart' as learning_dart_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learning_dart_homework.calculate()}!');
+}
---
+++ test/learning_dart_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:learning_dart_homework/learning_dart_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
